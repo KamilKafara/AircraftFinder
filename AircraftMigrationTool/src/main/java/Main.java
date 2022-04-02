ac68dfed8fdef6682ff5665db3abf43ec756c52c
Kamil Kafara <kafarakamil95@gmail.com>
2022-04-02 11:38:59 +0200

    AIR-2 Migration Tool
    + initial commit

---
+++ AircraftMigrationTool/src/main/java/Main.java
@@ -0,0 +1,12 @@
+import com.google.common.base.Stopwatch;
+
+class Main {
+    public static void main(String[] args) {
+        Stopwatch stopwatch = Stopwatch.createStarted();
+        System.out.println(stopwatch);
+
+        String filename = "";
+
+    }
+}
+
